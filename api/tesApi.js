e81387679327777e4c20609c54439caad6b2ea94
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2026-02-03 13:00:10 +0700

    delete comment

--- api/tesApi.js
+++ api/tesApi.js
@@ -1,6 +1,5 @@
 module.exports = (req, res) => {
   const origin = req.headers.origin || '*';
-  // Strip trailing slash from ALLOWED_ORIGIN to match browser origin exactly
   const allowedOrigin = (process.env.ALLOWED_ORIGIN || origin || '*').replace(/\/+$/, '');
 
   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
